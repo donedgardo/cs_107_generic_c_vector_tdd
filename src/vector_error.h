4da25bb1d3bd10055c76e4de2706877eac04ac72
Edgardo Carreras <edgardo.g.carreras@gmail.com>
2025-08-24 01:01:52 -0400

    Fix Build: Add vector_error file to git

---
+++ src/vector_error.h
@@ -0,0 +1,19 @@
+#ifndef VECTOR_ERROR_H
+#define VECTOR_ERROR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+static inline void vector_error(const char* message) {
+    fprintf(stderr, "%s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+static inline void vector_assert(bool condition, const char* message) {
+    if (condition) {
+        vector_error(message);
+    }
+}
+#endif
+
